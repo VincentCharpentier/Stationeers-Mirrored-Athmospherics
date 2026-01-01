d4d28366ab1d1c146436ce310893c2ab78af6b94
Charpentier Vincent <VincentCharpentier@users.noreply.github.com>
2026-01-01 16:10:11 +0100

    bump 0.5.2

--- MirroredAtmospherics.cs
+++ MirroredAtmospherics.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace MirroredAtmospherics.Scripts
 {
-    [BepInPlugin("net.Apolo.stationeers.MirroredAtmospherics.Scripts", "Mirrored Atmospherics", "0.0.5.0")]   
+    [BepInPlugin("net.Apolo.stationeers.MirroredAtmospherics.Scripts", "Mirrored Atmospherics", "0.0.5.2")]   
     public class MirroredAtmosphericsPlugin : BaseUnityPlugin
     {
         public static MirroredAtmosphericsPlugin Instance;
